3fc399276e134cd676181bee228c883f56cdf453
MakarandPundlik <mmp2308@gmail.com>
2021-10-03 11:48:14 +0530

    floats and integers

---
+++ Prog02.py
@@ -0,0 +1,32 @@
+# working with numbers and integers
+
+num = 3
+print(type(num)) #returns type of object
+
+# floor division 3//2
+# exponent 3**2
+
+num = 3.43//2.65
+print(num)
+num = 3/2 # returns float by default in python 3 
+          # returns integer in version < 3
+print(num)
+
+num = 3**2
+print(num)
+# all other operators are same as Java
+
+print(abs(-4)) # returns absolute values
+
+print(round(3.75)) #return nearest integer 
+print(round(3.25,1)) # return nth digit after the decimal 
+
+# all conditional operators are same 
+
+# same as Java String+String = String
+# string + int = string
+
+num1 = '100'
+num2 = '200'
+
+print(int(num1)+int(num2)) # parseInt = int(string as a parameter) 

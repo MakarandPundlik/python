003cd343031e7d7e37dcc9f142dc2882e42da4d6
MakarandPundlik <mmp2308@gmail.com>
2021-10-04 20:59:09 +0530

    Program in dictionary

---
+++ Prog04.py
@@ -0,0 +1,11 @@
+# dictionaries 
+# same as hashmap in Java
+
+student = {'name':'Makarand','age':22,'courses':['DSA','OS','DBMS','Digital Communication']}
+
+print(student)
+
+#to access individual keys
+print(student['name'])
+
+#keys can be any mutable data type
